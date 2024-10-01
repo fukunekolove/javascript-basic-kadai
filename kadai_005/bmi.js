726f2efc80c48a229fbf9d7c4f73d68e5c11e3e4
fukunekolove <greenpoco29@gmail.com>
2024-10-01 14:36:28 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//体重の変数の宣言
+let weight;
+
+//値の代入
+weight = 68;
+
+//身長の変数の宣言
+let height;
+
+//値の代入
+height = 1.7
+
+//BMIの計算
+let bmi = weight / (height*height);
+
+//コンソールへの出力
+console.log(bmi);
